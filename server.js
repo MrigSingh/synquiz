24768db3aa8d42debc3a681d120d080f1e9c6a55
MrigSingh <singh.mriganshu@gmail.com>
2020-06-07 13:14:39 +0530

    modified server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const cors = require("cors");
+const path = require("path")
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -30,6 +31,11 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
